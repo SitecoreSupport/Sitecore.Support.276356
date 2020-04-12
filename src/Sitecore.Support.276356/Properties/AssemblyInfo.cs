4ff997f9a8b1ee06ed4c23bf0d481c59fa62522b
SC-AndreyMolchanov <amch@sitecore.net>
2020-04-12 14:20:51 +0300

    commit initial code

---
+++ src/Sitecore.Support.276356/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.276356")]
+[assembly: AssemblyProduct("Sitecore.Support.276356")]
+[assembly: ComVisible(false)]
